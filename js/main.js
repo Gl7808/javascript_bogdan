1292c7805605737415e32a91fef71ea771e7a166
Vladislav <whorshunter42@yandex.ru>
2023-01-03 13:40:58 +0300

    - javascript vanila with bogdan | object start

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// . добавить/изменить
+// delete - удалить из объекта
+
+
+
+const MyTable = {
+  color: "black",
+  creator: "ikea",
+  price: 2400,
+  isGood: true,
+  characters: {
+    long: 200,
+    width: 70,
+    material: 'wood'
+  }
+}
+
+
+console.log(MyTable.characters);
